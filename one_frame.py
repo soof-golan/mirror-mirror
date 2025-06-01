3c2554c9e332dfc09cbcc5715584d3d212ffbc61
Soof Golan <hi@soofgolan.com>
2025-06-01 23:21:57 +0300

    timeit only diffuse

--- one_frame.py
+++ one_frame.py
@@ -8,22 +8,24 @@ seed = 42
 weight_type = torch.float16  # or float32
 
 # Load model.
-pipe = StableDiffusionPipeline.from_pretrained(repo, torch_dtype=weight_type)
+pipe: StableDiffusionPipeline = StableDiffusionPipeline.from_pretrained(
+    repo, torch_dtype=weight_type
+)
 pipe.to("cuda")
 
 prompt = "a close-up picture of an old man standing in the rain"
+g = torch.Generator(device="cuda").manual_seed(seed)
 
 
 # Ensure using 1 inference step and CFG set to 0.
 def gen():
-    image = pipe(
+    image = pipe.__call__(
         prompt,
         num_inference_steps=1,
         guidance_scale=0,
-        generator=torch.Generator(device="cuda").manual_seed(seed),
+        generator=g,
+        output_type="latent",
     ).images[0]
-
-    image.save("output.png")
 
 
 t = timeit.timeit("gen()", globals=globals(), number=10)
