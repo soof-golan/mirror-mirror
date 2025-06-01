9429edddcb61477eef0b44f02933f1b1ce68df8f
Soof Golan <hi@soofgolan.com>
2025-06-01 21:45:36 +0300

    timeit

--- one_frame.py
+++ one_frame.py
@@ -1,3 +1,5 @@
+import timeit
+
 import torch
 from diffusers import StableDiffusionPipeline
 
@@ -11,12 +13,18 @@ pipe.to("cuda")
 
 prompt = "a close-up picture of an old man standing in the rain"
 
-# Ensure using 1 inference step and CFG set to 0.
-image = pipe(
-    prompt,
-    num_inference_steps=1,
-    guidance_scale=0,
-    generator=torch.Generator(device="cuda").manual_seed(seed),
-).images[0]
 
-image.save("output.png")
+# Ensure using 1 inference step and CFG set to 0.
+def gen():
+    image = pipe(
+        prompt,
+        num_inference_steps=1,
+        guidance_scale=0,
+        generator=torch.Generator(device="cuda").manual_seed(seed),
+    ).images[0]
+
+    image.save("output.png")
+
+
+t = timeit.timeit("gen()", globals=globals(), number=10)
+print(f"Time taken for 10 runs: {t:.2f} seconds")
