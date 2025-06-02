b725332b5170adf895af4e08e66d46440e4c4806
Soof Golan <hi@soofgolan.com>
2025-06-02 17:50:20 +0300

    precache prompt

--- one_frame.py
+++ one_frame.py
@@ -15,19 +15,26 @@ pipe.to("cuda")
 
 prompt = "a close-up picture of an old man standing in the rain"
 g = torch.Generator(device="cuda").manual_seed(seed)
+prompt_embed = pipe.encode_prompt(
+    [prompt],
+    num_images_per_prompt=1,
+    device=pipe.device,
+    do_classifier_free_guidance=False,
+)
 
 
 # Ensure using 1 inference step and CFG set to 0.
 def gen():
     image = pipe.__call__(
-        prompt,
+        # prompt,
+        prompt_embeds=prompt_embed,
         num_inference_steps=1,
         guidance_scale=0,
-        generator=g,
+        # generator=g,
         output_type="latent",
     ).images[0]
 
 
 n = 100
 t = timeit.timeit("gen()", globals=globals(), number=n)
-print(f"Time taken for 10 runs: {t:.2f} seconds ({t / n:.2f} seconds per run)")
+print(f"Time taken for {n} runs: {t:.2f} seconds ({t / n:.2f} seconds per run)")
